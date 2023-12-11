4dc528e0e3e8588c4cdb1c72987bb74f34918e01
oeangg <msubhan33@gmail.com>
2023-12-11 14:23:41 +0700

    js

--- public/script.js
+++ public/script.js
@@ -3,8 +3,10 @@ const toggleMenu = () => {
   const burgerMenu = document.querySelector(".menu-icon");
   const src = burgerMenu.getAttribute("src");
 
-  const isBurger = src === "img/hamburger-menu.svg";
-  const iconName = isBurger ? "img/close-icon.svg" : "img/hamburger-menu.svg";
+  const isBurger = src === "public/img/hamburger-menu.svg";
+  const iconName = isBurger
+    ? "public/img/close-icon.svg"
+    : "public/img/hamburger-menu.svg";
 
   burgerMenu.setAttribute("src", iconName);
 
